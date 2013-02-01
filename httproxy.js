5b02bc3eac000c5e41227eea92fc7a2ed9fc4b89
Jonas Magazinius <jonas.magazinius@chalmers.se>
2013-02-01 17:28:31 +0100

    Updates to the webproxy adding cache support, fixes and new stuff!

--- httproxy.js
+++ httproxy.js
@@ -7,7 +7,7 @@
 // Configuration
 var lib = 'lib',
  jsflow = 'jsflow/lib',
-  cache = 'cache';
+  cache = 'jsflow/proxy_data/overlay';
 
 var hostname = process.argv[2] || 'jsflow.monitor';
 http.port = process.argv[3] || 80;
@@ -19,7 +19,7 @@ https.options = {
 };
 
 var host = new RegExp("\\." + hostname.replace(/\./, '\\.'));
-var url = /^(?:https?:)?\/\/([.\-a-zA-Z0-9]+)/;
+var url = /^((?:https?:)?\/\/[.\-a-zA-Z0-9]+)/;
 var events = [
   'onload',
   'onunload',
@@ -49,7 +49,7 @@ var events = [
 var proxyInline = function(nodes, attr) {
   for (var i = 0; i < nodes.length; i++) {
     if (url.test(nodes[i][attr]))
-      nodes[i][attr] = nodes[i][attr].replace(url, 'http://$1.' + hostname);
+      nodes[i][attr] = nodes[i][attr].replace(url, '$1.' + hostname);
   }
 }
 
@@ -58,11 +58,44 @@ var eventInline = function(nodes, attr) {
     nodes[i]._attrsByQName[attr].data = 'Monitor.handleEvent(\'' + nodes[i]._attrsByQName[attr].data.replace(/\\/g, '\\\\').replace(/'/g, '\\\'') + '\', event)'
 }
 
+var inlineHtml = function (document) {
+  // TODO: Seems to be some kind of problem with the line below, not all tags are selected
+  proxyInline(document.querySelectorAll('a'), 'href');
+  proxyInline(document.querySelectorAll('script[src]'), 'src');
+  proxyInline(document.querySelectorAll('form[action]'), 'action');
+  proxyInline(document.querySelectorAll('iframe[src]'), 'src');
+  proxyInline(document.querySelectorAll('frame[src]'), 'src');
+  proxyInline(document.querySelectorAll('area[href]'), 'href');
+  // TODO: Deal with OBJECT and EMBED tags
+  // TODO: Insert form[onsubmit] action to catch form submits in monitor
+
+  var scripts = document.querySelectorAll('script');
+  // DONE: Handle inline scripts
+  for (var i = 0; i < scripts.length; i++) {
+    if (scripts[i].src === '/')
+      scripts[i].text = 'Monitor.evaluate("' + scripts[i].text.replace(/^<!--/, '').replace(/\\/g, '\\\\').replace(/"/g, '\\"').replace(/\r?\n/g, "\\n\\\n") + '")';
+  }
+
+  // DONE: Handle all event handlers
+  for (var key in events)
+    eventInline(document.querySelectorAll('[' + events[key] + ']'), events[key]);
+
+  // DONE: Add monitor
+  var bootstrap = document.createElement('script');
+  bootstrap.src = 'http://' + hostname + '/bootstrap.js';
+  document.head.insertBefore(bootstrap, document.head.firstChild);
+  var monitor = document.createElement('script');
+  monitor.src = 'http://' + hostname + '/monitor.js';
+  document.head.insertBefore(monitor, bootstrap);
+
+  return document.innerHTML;
+}
+
 // TODO: Add logging
 var proxy = function (req, res) {
   try {
-    res = new CachedWrite(res);
-
+    //res = new CachedWrite(res);
+    var cached = cache + '/' + req.headers.host.replace(host, '') + req.url + '.' + req.method;
     // DONE: Serve monitor files
     if (req.headers['host'] === hostname) {
       // DONE: Prevent directory traversal attacks
@@ -79,26 +112,43 @@ var proxy = function (req, res) {
       }
       res.end();
 
-    } else if (fs.existsSync(cache + '/' + req.headers.host + req.url.replace(/[\\\/:\?\*"<>|]/g, function ($1) { return '%' + $1.charCodeAt(0).toString(16) }))) {
+    } else if (fs.existsSync(cached + '.data')) {
 
       console.log('Cached: ' + req.headers['host'] + req.url);
       // DONE: Add caching
-      // TODO: Read cached headers
+      // DONE: Read cached headers
       // DONE: Read cached page
-      res.writeHead(200, {});
-      res.write(fs.readFileSync(cache + '/' + req.headers.host + req.url.replace(/[\\\/:\?\*"<>|]/g, function ($1) { return '%' + $1.charCodeAt(0).toString(16) })))
-      // TODO: Serialize?
+      var headers = JSON.parse(fs.readFileSync(cached + '.header').toString()),
+          data = fs.readFileSync(cached + '.data').toString();
+      if (/html/.test(headers['content-type']))
+        data = inlineHtml(domino.createDocument(data));
+      else if (/javascript/.test(headers['content-type']))
+        data = 'Monitor.evaluate("' +
+                data.toString('utf8').replace(/\\/g, '\\\\').replace(/"/g, '\\"').replace(/\r?\n/g, "\\n\\\n") +
+                '")';
+      // DONE: Disable caching (at least temporarily)
+      delete headers['last-modified'];
+      delete headers['expires'];
+      delete headers['etag'];
+      delete headers['age'];
+      delete headers['cache-control'];
+      headers['content-length'] = data.length;
+      res.writeHead(200, headers);
+      res.write(data);
+      // TODO?: Serialize
       res.end();
+
     } else {
 
       console.log('Proxying: ' + req.headers['host'] + req.url);
 
       // DONE: if lastchar == / then lastchar = /index.html
-      try {
-        res.fd = fs.openSync(cache + '/' + req.headers.host + req.url.replace(/[\\\/:\?\*"<>|]/g, function ($1) { return '%' + $1.charCodeAt(0).toString(16) }), 'wx');
-      } catch (e) { console.log('Crashed here: ' + e) }
+      //      try {
+      //        res.fd = fs.openSync(cache + '/' + req.headers.host + req.url.replace(/[\\\/:\?\*"<>|]/g, function ($1) { return '%' + $1.charCodeAt(0).toString(16) }), 'wx');
+      //      } catch (e) { console.log('Crashed here: ' + e) }
 
       // DONE: Strip headers (prevent encoding and disable caching)
+      // TODO?: Support encoding
       delete req.headers['accept-encoding'];
       delete req.headers['if-modified-since'];
       delete req.headers['if-none-match'];
@@ -115,7 +165,7 @@ var proxy = function (req, res) {
 
         // DONE: Rewrite redirects (status 302)
         if (pres.headers['location'])
-          pres.headers['location'] = pres.headers['location'].replace(url, 'http://$1.' + hostname);
+          pres.headers['location'] = pres.headers['location'].replace(url, '$1.' + hostname);
 
         // DONE: Disable caching (at least temporarily)
         delete pres.headers['last-modified'];
@@ -129,37 +179,8 @@ var proxy = function (req, res) {
           var parser = new domino.Parser();
           parser.parse(pres);
           parser.on('end', function () {
-            var document = parser.document();
 
-            // TODO: Seems to be some kind of problem with the line below, not all tags are selected
-            proxyInline(document.querySelectorAll('a'), 'href');
-            proxyInline(document.querySelectorAll('script[src]'), 'src');
-            proxyInline(document.querySelectorAll('form[action]'), 'action');
-            proxyInline(document.querySelectorAll('iframe[src]'), 'src');
-            proxyInline(document.querySelectorAll('frame[src]'), 'src');
-            proxyInline(document.querySelectorAll('area[href]'), 'href');
-            // TODO: Deal with OBJECT and EMBED tags
-
-            var scripts = document.querySelectorAll('script');
-            // DONE: Handle inline scripts
-            for (var i = 0; i < scripts.length; i++) {
-              if (scripts[i].src === '/')
-                scripts[i].text = 'Monitor.evaluate("' + scripts[i].text.replace(/^<!--/, '').replace(/\\/g, '\\\\').replace(/"/g, '\\"').replace(/\r?\n/g, "\\n\\\n") + '")';
-            }
-
-            // DONE: Handle all event handlers
-            for (var key in events)
-              eventInline(document.querySelectorAll('[' + events[key] + ']'), events[key]);
-
-            // DONE: Add monitor
-            var bootstrap = document.createElement('script');
-            bootstrap.src = 'http://' + hostname + '/bootstrap.js';
-            document.head.insertBefore(bootstrap, document.head.firstChild);
-            var monitor = document.createElement('script');
-            monitor.src = 'http://' + hostname + '/monitor.js';
-            document.head.insertBefore(monitor, bootstrap);
-
-            var html = document.innerHTML;
+            var html = inlineHtml(parser.document());
             pres.headers['content-length'] = html.length;
             res.writeHead(pres.statusCode, pres.headers);
             res.write(html);
@@ -172,14 +193,14 @@ var proxy = function (req, res) {
           // TODO: Calculate the new length instead of deleting the content-length header
           delete pres.headers['content-length'];
           res.writeHead(pres.statusCode, pres.headers);
-          res.write('try{Monitor.evaluate("');
+          res.write('Monitor.evaluate("');
 
           pres.on('data', function (chunk) {
             res.write(chunk.toString('utf8').replace(/\\/g, '\\\\').replace(/"/g, '\\"').replace(/\r?\n/g, "\\n\\\n"));
           });
 
           pres.on('end', function () {
-            res.write('")}catch(e){console.log(e.stack)}');
+            res.write('")');
             res.end();
           });
 
@@ -221,25 +242,25 @@ var proxy = function (req, res) {
   }
 }
 
-function CachedWrite(res) {
+//function CachedWrite(res) {
 
-  this.writeHead = function (status, headers) {
-    res.writeHead(status, headers);
-  }
-  this.write = function (text) {
-    res.write(text);
-    if (this.fd)
-      fs.write(this.fd, text);
-  }
-  this.end = function (text) {
-    res.end(text);
-    if (this.fd) {
-      fs.write(this.fd, text);
-      fs.close(this.fd);
-    }
-  }
+//  this.writeHead = function (status, headers) {
+//    res.writeHead(status, headers);
+//  }
+//  this.write = function (text) {
+//    res.write(text);
+//    if (this.fd)
+//      fs.write(this.fd, text);
+//  }
+//  this.end = function (text) {
+//    res.end(text);
+//    if (this.fd) {
+//      fs.write(this.fd, text);
+//      fs.close(this.fd);
+//    }
+//  }
 
-}
+//}
 
 // DONE: Add HTTPS support
 // TODO: Listen on all ports
